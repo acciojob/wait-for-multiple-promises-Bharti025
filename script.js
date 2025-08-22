670591c3115cd5f73628f5327498910574ae510b
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-22 08:42:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const tbody=document.getElementById("output");
+function promise1=()=>{
+return new Promise((resolve,reject)=>{
+setTimeout(()=>{
+ resolve("Promise 1");
+});
+});
+};
+function promise2=()=>{
+return new Promise((resolve,reject)=>{
+setTimeout(()=>{
+ resolve("Promise 2");
+});
+});
+};
+function promise3=()=>{
+return new Promise((resolve,reject)=>{
+setTimeout(()=>{
+ resolve("Promise 3");
+});
+});
+}
+Promise.all([promise1(),promise2(),promise3()])
+.then((data)=>{
+	let row=document.createElement("tr");
+	let col1=document.createElement("td");
+     let col2=document.createElement("td");
+	row.append(col1);
+	row.append(col2);
+    output.append(row);
+	
+})
